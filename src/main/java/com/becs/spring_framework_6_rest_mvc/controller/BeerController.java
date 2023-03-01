e2836bd03e12c0aaf17e804b5be2eaa3c739b988
Norbert Becs <becs.norbert@gmail.com>
2023-03-01 20:31:47 +0100

    New BeerService method

--- src/main/java/com/becs/spring_framework_6_rest_mvc/controller/BeerController.java
+++ src/main/java/com/becs/spring_framework_6_rest_mvc/controller/BeerController.java
@@ -4,16 +4,23 @@ import com.becs.spring_framework_6_rest_mvc.model.Beer;
 import com.becs.spring_framework_6_rest_mvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
-@Controller
 @AllArgsConstructor
+@RestController
 public class BeerController {
 
     private final BeerService beerService;
+
+    @RequestMapping("/api/v1/beer")
+    public List<Beer> listBeers() {
+        return beerService.listBeers();
+    }
 
     public Beer getBeerById(UUID id) {
 
@@ -23,3 +30,4 @@ public class BeerController {
     }
 
 }
+
--- src/main/java/com/becs/spring_framework_6_rest_mvc/services/BeerService.java
+++ src/main/java/com/becs/spring_framework_6_rest_mvc/services/BeerService.java
@@ -2,9 +2,12 @@ package com.becs.spring_framework_6_rest_mvc.services;
 
 import com.becs.spring_framework_6_rest_mvc.model.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
+
+    List<Beer> listBeers();
 
     Beer getBeerById(UUID id);
 

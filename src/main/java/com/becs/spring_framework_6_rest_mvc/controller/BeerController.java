3348b537ab2e92c9c378ba2cb4cc6837943e085b
Norbert Becs <becs.norbert@gmail.com>
2023-03-01 20:57:02 +0100

    New BeerService method

--- src/main/java/com/becs/spring_framework_6_rest_mvc/controller/BeerController.java
+++ src/main/java/com/becs/spring_framework_6_rest_mvc/controller/BeerController.java
@@ -4,8 +4,7 @@ import com.becs.spring_framework_6_rest_mvc.model.Beer;
 import com.becs.spring_framework_6_rest_mvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -13,20 +12,24 @@ import java.util.UUID;
 @Slf4j
 @AllArgsConstructor
 @RestController
+@RequestMapping("/api/v1/beer")
 public class BeerController {
 
     private final BeerService beerService;
 
-    @RequestMapping("/api/v1/beer")
+    //@RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public List<Beer> listBeers() {
         return beerService.listBeers();
     }
 
-    public Beer getBeerById(UUID id) {
+    //@RequestMapping(value = "/api/v1/beer/{beerId}", method = RequestMethod.GET)
+    @GetMapping("/{beerId}")
+    public Beer getBeerById(@PathVariable("beerId") UUID beerId) {
 
-        log.debug("Get Beer by Id - in controller. Id: {}", id.toString());
+        log.debug("Get Beer by Id - in controller. Id: {}", beerId.toString());
 
-        return beerService.getBeerById(id);
+        return beerService.getBeerById(beerId);
     }
 
 }
